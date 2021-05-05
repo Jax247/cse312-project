ac9b36ae8f6772c94908f40e1b305eda6e4f86e3
jemajews <36274776+jemajews@users.noreply.github.com>
2021-05-04 21:32:17 -0400

    Deploy fixes

--- server/server.js
+++ server/server.js
@@ -78,8 +78,8 @@ let allUsers = new Map();
 let tokenUsers = new Map();
 
 
-var url = "mongodb://localhost:27017/";
-//var url = 'mongodb://mongo:27017';
+//var url = "mongodb://localhost:27017/";
+var url = 'mongodb://mongo:27017';
 
 MongoClient.connect(url, function (err, db) {
     if (err) throw err;
